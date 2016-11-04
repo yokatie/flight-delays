3ed16ac232d03e1c66cbaaebf526727f75478554
jacopotagliabue <tagliabue.jacopo@gmail.com>
2016-11-03 18:33:55 -0600

    move over from notebook the main converter logic + some local test

--- decision_tree_converter/converter.py
+++ decision_tree_converter/converter.py
@@ -13,6 +13,7 @@ import sys
 import getopt
 from spark_python_dtree_converter import SparkPythonModelConverter
 
+
 def explain_usage():
     print ''
     return
@@ -40,6 +41,10 @@ def main(argv):
             # output the conversion as a python class (.py class), not pickled object
             to_pickle = False
 
+    # test values
+    #spark_model_file = '/Users/jacopo/Documents/spark/decision_tree.txt'
+    #output_file = '/Users/jacopo/Documents/spark/decision_tree.py'
+    #to_pickle = False
     # make the conversion with the provided options
     converter = SparkPythonModelConverter()
     converter.convert(spark_model_file, output_file, to_pickle)
--- decision_tree_converter/spark_python_dtree_converter.py
+++ decision_tree_converter/spark_python_dtree_converter.py
@@ -2,7 +2,79 @@
 
 class SparkPythonModelConverter:
 
+    BASE_INDENTATION = 4 * 2  # 4 is at function level, 8 is when inside a class
+
     def convert(self, spark_model_file, output_file, to_pickle):
+        python_class_code = self.translate_tree(spark_model_file, self.BASE_INDENTATION)
+        # if needs to be pickled, instantiate the class and pickle it
+        if to_pickle:
+            return
+        # otherwise, save the file as a .py file
+        else:
+            self.dump_python_class(output_file, python_class_code)
 
         return
 
+    def dump_python_class(self, output_file, python_class_code):
+        with open(output_file, 'w') as f:
+            f.write(python_class_code)
+
+        return
+
+    def parse_line(self, line):
+        # each line is made by: conditional instruction + ( + feature + operator + number|list +)
+        # eliminate insignicant spaces and remove parenthesis
+        line = line.replace('feature ', 'feature').replace('(', '').replace(')', '')
+        return line.lstrip().split()
+
+    def translate_split_line(self, parsed_line):
+        # this is a split, so a if clause
+        # instructions are the same in Python, just need to lower case
+        instruction = parsed_line[0].lower().replace('else', 'elif')
+        # features need to be replaced with an item from the feature array
+        feature = parsed_line[1].replace('feature', 'features[') + ']'
+        operator = ' '.join(parsed_line[2:-1])
+        # if it is a number, it's just the target itself, if it is a list, need to replace parenthesis
+        variable = parsed_line[-1].replace('{', '[').replace('}', ']')
+
+        return '{0} {1} {2} {3}:\n'.format(instruction, feature, operator, variable)
+
+    def translate_prediction_line(self, parsed_line):
+        # this is the end of the tree, just return the target category
+        return 'return {0}\n'.format(parsed_line[1])
+
+    def translate_parsed_line(self, parsed_line):
+        # each split line is [ instruction, feature, operator, number|list ]
+        # each prediction line is [ instruction, prediction ]
+        # find out which type of line is and translate accordingly
+        if parsed_line[0] == 'Predict:':
+            return self.translate_prediction_line(parsed_line)
+        elif parsed_line[0] in ['If', 'Else']:
+            return self.translate_split_line(parsed_line)
+        else:
+            raise 'Instruction not recognized'
+
+    def get_indentation_length(self,line):
+        return len(line) - len(line.lstrip())
+
+    def translate_tree(self, input_file, base_indentation):
+        final_code = []
+        # get lines from file and skip header
+        lines = []
+        with open(input_file) as f:
+            lines = [line for line in f][1:]
+        # get start_indentation
+        start_indentation = self.get_indentation_length(lines[0])
+        # loop over the lines and parse them
+        for line in lines:
+            line_indentation = self.get_indentation_length(line) - start_indentation
+            indentation = ' ' * (base_indentation + (line_indentation * 2))
+            final_code.append(indentation + self.translate_parsed_line(self.parse_line(line)))
+
+        return self.get_class_and_function_header() + ''.join(final_code)
+
+    def get_class_and_function_header(self):
+        class_header = 'class DecisionTree:\n\n'
+        function_header = '    def classify(features):\n'
+
+        return class_header + function_header
